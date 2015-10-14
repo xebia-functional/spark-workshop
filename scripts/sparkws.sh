d82fad8282fb81ac012ae99f2857eca58faac040
Juan Pedro Moreno <juanpmorest@gmail.com>
2015-10-14 11:19:48 +0200

    Replaces Spark Master URL

--- scripts/sparkws.sh
+++ scripts/sparkws.sh
@@ -7,7 +7,7 @@ sh $SPARK_HOME/sbin/stop-slave.sh
 # Start the master:
 sh $SPARK_HOME/sbin/start-master.sh
 # Start the slaves:
-sh $SPARK_HOME/sbin/start-slave.sh spark://macjp.local:7077
+sh $SPARK_HOME/sbin/start-slave.sh spark://localhost:7077
 
 # Start the shell:
-sh $SPARK_HOME/bin/spark-shell --master spark://macjp.local:7077 --total-executor-cores 4
+sh $SPARK_HOME/bin/spark-shell --master spark://localhost:7077 --total-executor-cores 4
