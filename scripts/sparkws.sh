46ec0367ef0b0108979ef0111ad6db3fbd1ed23e
Juan Pedro Moreno <juanpmorest@gmail.com>
2015-10-15 20:45:10 +0200

    Attaches the master machine to the localhost IP

--- scripts/sparkws.sh
+++ scripts/sparkws.sh
@@ -5,9 +5,9 @@ sh $SPARK_HOME/sbin/stop-master.sh
 sh $SPARK_HOME/sbin/stop-slave.sh
 
 # Start the master:
-sh $SPARK_HOME/sbin/start-master.sh
+sh $SPARK_HOME/sbin/start-master.sh -i 127.0.0.1
 # Start the slaves:
-sh $SPARK_HOME/sbin/start-slave.sh spark://localhost:7077
+sh $SPARK_HOME/sbin/start-slave.sh spark://127.0.0.1:7077
 
 # Start the shell:
-sh $SPARK_HOME/bin/spark-shell --master spark://localhost:7077 --total-executor-cores 4
+sh $SPARK_HOME/bin/spark-shell --master spark://127.0.0.1:7077 --total-executor-cores 4
