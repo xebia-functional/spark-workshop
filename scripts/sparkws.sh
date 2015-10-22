77574eff512fa52bcad1fd9c7d19913d408ac773
Juan Pedro Moreno <juanpmorest@gmail.com>
2015-10-22 10:56:37 +0200

    Fixes script issues/4

--- scripts/sparkws.sh
+++ scripts/sparkws.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 # Stop the current instances:
-sh $SPARK_HOME/sbin/stop-master.sh
-sh $SPARK_HOME/sbin/stop-slave.sh
+$SPARK_HOME/sbin/stop-master.sh
+$SPARK_HOME/sbin/stop-slave.sh
 
 # Start the master:
-sh $SPARK_HOME/sbin/start-master.sh -i 127.0.0.1
+$SPARK_HOME/sbin/start-master.sh -i 127.0.0.1
 # Start the slaves:
-sh $SPARK_HOME/sbin/start-slave.sh spark://127.0.0.1:7077
+$SPARK_HOME/sbin/start-slave.sh spark://127.0.0.1:7077
 
 # Start the shell:
-sh $SPARK_HOME/bin/spark-shell --master spark://127.0.0.1:7077 --total-executor-cores 4
+$SPARK_HOME/bin/spark-shell --master spark://127.0.0.1:7077 --total-executor-cores 4
